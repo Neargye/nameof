c6c6112161d726a7673feacf69e6b6e4782c6e84
Balazs Benics <benicsbalazs@gmail.com>
2019-07-28 15:37:33 +0200

    try ConanMultiPacker

--- build.py
+++ build.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bincrafters import build_template_header_only
+from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = build_template_header_only.get_builder()
-    additional_options = { 'nameof:build_tests': True, 'nameof:build_examples': True }
-    builder.update_build_if(lambda build: True, new_options=additional_options)
+    builder = ConanMultiPackager()
+    builder.add_common_builds()
+    updated_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        options['prometheus-cpp:mode'] = 'pull'
+        updated_builds.append([settings, options, env_vars, build_requires])
+        options = options.copy()
+        options['prometheus-cpp:mode'] = 'push'
+        updated_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = updated_builds
     builder.run()
