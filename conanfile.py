7276880fdfb3b800efc3defe434771d94fbb4ecb
Balazs Benics <benicsbalazs@gmail.com>
2019-07-28 14:43:07 +0200

    fix non stdc++17 buildbots

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools, CMake
-import os
-
+from conans import ConanFile, CMake
+from conans.model.version import Version
+from conans.errors import ConanException
 
 class NameofConan(ConanFile):
     name = 'nameof'
@@ -32,15 +32,39 @@ class NameofConan(ConanFile):
         'build_examples': False,
     }
 
+    @property
+    def supports_string_view(self):
+        compiler = str(self.settings.compiler)
+        version = Version(str(self.settings.compiler.version))
+        if compiler == 'Visual Studio' and version >= Version('15'):
+            return True
+        if compiler == 'gcc' and version >= Version('7'):
+            return True
+        if compiler == 'icc' and version >= Version('19'):
+            return True
+        if compiler == 'clang' and version >= Version('5'):
+            return True
+        if compiler == 'apple-clang' and version >= Version('9'):
+            return True
+        return False
+
     def requirements(self):
         if self.options.build_tests:
             self.requires('Catch2/2.9.1@catchorg/stable')
 
+    def configure(self):
+        if not self.supports_string_view:
+            raise ConanException('The specified compiler must support C++17')
+
     def config_options(self):
         needs_build = self.options.build_tests or self.options.build_examples
+
+        # remove all build settings since it is a header-only library
         if not needs_build:
-            # remove all build settings since it is a header-only library
-            self.options.remove(['os', 'compiler', 'build_type', 'arch'])
+            del self.options.os
+            del self.options.compiler
+            del self.options.build_type
+            del self.options.arch
 
     def configure_cmake(self):
         cmake = CMake(self)
