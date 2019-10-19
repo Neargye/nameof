0f3dbd70f2dca3efe40e5dffe7af4217a6dd335e
neargye <neargye@gmail.com>
2019-10-19 16:27:54 +0500

    v0.9.2

--- include/nameof.hpp
+++ include/nameof.hpp
@@ -5,7 +5,6 @@
 // | |\  | (_| | | | | | |  __/ (_) | |   | |____|_|   |_|
 // |_| \_|\__,_|_| |_| |_|\___|\___/|_|    \_____|
 // https://github.com/Neargye/nameof
-// vesion 0.9.1
 // vesion 0.9.2
 //
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
